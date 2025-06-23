dc0231c7e8dbde38b7a0cdb2ce70c70bccd51f72
mrhihihi <gabriel.kyeremateng.gk@gmail.com>
2025-06-23 12:41:56 +0000

    feat: initialize dotenv setup in config folder

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log('server running on localhost:3000')
+import express from "express";
+
+import { PORT } from "./config/env.js";
+
+const app = express();
+
+app.get('/', (req,res) => {
+    res.send('Welcome to the Autobill Tracker API!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Autobill Tracker API is running on http://localhost:${PORT}`)
+})
+
+export default app;
