f5df3c3b26abf68ce4c2c1bf91bb0fa50153e6e3
mrhihihi <gabriel.kyeremateng.gk@gmail.com>
2025-06-24 16:45:47 +0000

    feat: implement centralized error-handling middleware for API

--- app.js
+++ app.js
@@ -6,12 +6,22 @@ import router from "./routes/index.routes.js";
 
 import connectToDatabase from "./database/mongodb.js";
 
+import { errorMiddleware } from "./middlewares/error.middleware.js";
+
+import cookieParser from "cookie-parser";
+
+
 const app = express();
 
 app.use(express.json());
 
+app.use(express.urlencoded( { extended: false }));
+
+app.use(cookieParser())
+
 app.use('/api/v1/',router);
 
+app.use(errorMiddleware);
 
 app.get('/', (req,res) => {
     res.send('Welcome to the Autobill Tracker API!');
